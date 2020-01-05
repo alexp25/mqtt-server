ae9be8509c93b6f7ca37e6e714e62187831ca0ed
alexp25 <predescu.alex93@gmail.com>
2020-01-05 11:25:33 +0200

    removed try catch blocks inside db methods, keep try catch in decorator, to catch db connection timeout and reconnect

--- modules/database.py
+++ modules/database.py
@@ -50,7 +50,6 @@ def PublisherProcess(q_in: Queue, q_out: Queue, conf):
         time.sleep(0.01)
         if not q_in.empty():
             s: Sensor = q_in.get()
-
             try:
                 publish_sensor_data_ext(s, connection, cursor)
             except pymysql.Error as e:
@@ -69,12 +68,18 @@ def _check_conn(func):
         try:
             return func(self, *args, **kwargs)
         except pymysql.Error as e:
+            self.logg.log(Utils.format_exception(self.__class__.__name__))
             if 'MySQL server has gone away' in str(e):
                 # reconnect MySQL
+                self.logg.log("attempt reconnect")
                 self.connect()
             else:
                 # No need to retry for other reasons
                 pass
+                return None
+        except Exception:
+            self.logg.log(Utils.format_exception(self.__class__.__name__))
+            return None
     return wrap
 
 
@@ -140,96 +145,68 @@ class Database:
 
     @_check_conn
     def get_topics(self):
-        try:
-            self.cursor.execute('select * from topic')
-            results = self.cursor.fetchall()
-            self.connection.commit()
-            return results
-        except:
-            self.logg.log(Utils.format_exception(self.__class__.__name__))
-            return None
-        finally:
-            self.connection.commit()
+        self.cursor.execute('select * from topic')
+        results = self.cursor.fetchall()
+        self.connection.commit()
+        return results
+        
 
     @_check_conn
     def get_sensors(self):
-        try:
-            self.cursor.execute(
-                'select sensor.sensor_id, sensor.log_rate, sensor.topic_code, sensor.sensor_type_code, topic.name as "topic_name" from sensor inner join topic on sensor.topic_code=topic.code')
-            results = self.cursor.fetchall()
-            self.connection.commit()
-            return results
-        except:
-            self.logg.log(Utils.format_exception(self.__class__.__name__))
-            return None
-        finally:
-            self.connection.commit()
+        self.cursor.execute(
+            'select sensor.sensor_id, sensor.log_rate, sensor.topic_code, sensor.sensor_type_code, topic.name as "topic_name" from sensor inner join topic on sensor.topic_code=topic.code')
+        results = self.cursor.fetchall()
+        self.connection.commit()
+        return results        
 
     @_check_conn
     def get_sensor_data(self, id, chan, limit):
-        try:
-            sql = 'select * from (select * from sensor_data where sensor_id=%s and chan=%s order by id DESC limit %s) as data_desc order by data_desc.id ASC'
-            params = (int(id), int(chan), int(limit))
-            self.logg.log(sql)
-            self.logg.log(params)
-            self.cursor.execute(sql, params)
-            results = self.cursor.fetchall()
-            self.connection.commit()
-            return results
-        except:
-            self.logg.log(Utils.format_exception(self.__class__.__name__))
-            return None
-        finally:
-            self.connection.commit()
-
+        sql = 'select * from (select * from sensor_data where sensor_id=%s and chan=%s order by id DESC limit %s) as data_desc order by data_desc.id ASC'
+        params = (int(id), int(chan), int(limit))
+        self.logg.log(sql)
+        self.logg.log(params)
+        self.cursor.execute(sql, params)
+        results = self.cursor.fetchall()
+        self.connection.commit()
+        return results
+        
     @_check_conn
     def create_sensor(self, sensor):
-        try:
-            sdata: MQTTMessage = sensor.current_data
-            # self.logg.log(sdata.__dict__)
-            if not sdata:
-                return None
-
-            self.logg.log("create sensor for topic: " + sensor.topic_name +
-                          " (code " + str(sensor.topic_code) + ")")
-            self.cursor.execute(
-                'select * from topic where name=%s', (sensor.topic_name,))
-            topic = self.cursor.fetchone()
-            self.logg.log(
-                "topic[" + str(sensor.topic_name) + "]: " + str(topic))
-            # self.logg.log(topic["id"])
-            sensor.id = Utils.get_sensor_id_encoding(
-                sensor.raw_id, topic["code"])
-            sql = "INSERT INTO sensor (sensor_id, log_rate, topic_code) VALUES (%s, %s, %s)"
-            sensor.log_rate = topic["log_rate"]
-            sensor.topic_code = topic["code"]
-
-            self.logg.log("sensor: " + str(sensor.__dict__))
-            params = (sensor.id, sensor.log_rate, topic["code"])
-            self.logg.log(sql + str(params))
-            self.cursor.execute(sql, params)
-            # commit the changes to the database
-            self.connection.commit()
-            # close communication with the database
-            # self.cursor.close()
-            return sensor
-        except:
-            self.logg.log(Utils.format_exception(self.__class__.__name__))
+        sdata: MQTTMessage = sensor.current_data
+        # self.logg.log(sdata.__dict__)
+        if not sdata:
             return None
-        finally:
-            try:
-                self.connection.commit()
-            except:
-                self.logg.log(Utils.format_exception(self.__class__.__name__))
 
+        self.logg.log("create sensor for topic: " + sensor.topic_name +
+                        " (code " + str(sensor.topic_code) + ")")
+        self.cursor.execute(
+            'select * from topic where name=%s', (sensor.topic_name,))
+        topic = self.cursor.fetchone()
+        self.logg.log(
+            "topic[" + str(sensor.topic_name) + "]: " + str(topic))
+        # self.logg.log(topic["id"])
+        sensor.id = Utils.get_sensor_id_encoding(
+            sensor.raw_id, topic["code"])
+        sql = "INSERT INTO sensor (sensor_id, log_rate, topic_code) VALUES (%s, %s, %s)"
+        sensor.log_rate = topic["log_rate"]
+        sensor.topic_code = topic["code"]
+
+        self.logg.log("sensor: " + str(sensor.__dict__))
+        params = (sensor.id, sensor.log_rate, topic["code"])
+        self.logg.log(sql + str(params))
+        self.cursor.execute(sql, params)
+        # commit the changes to the database
+        self.connection.commit()
+        # close communication with the database
+        # self.cursor.close()
+        return sensor
+    
     @_check_conn
     def publish_sensor_data_core(self, s: Sensor):
         self.logg.log("publish data")
         self.logg.log(s.__dict__)
-        try:
-            publish_sensor_data_ext(s, self.connection, self.cursor)
-        except:
-            self.logg.log(Utils.format_exception(self.__class__.__name__))
+        publish_sensor_data_ext(s, self.connection, self.cursor)
+       
 
     def publish_sensor_data(self, s: Sensor):
         if Constants.conf["ENV"]["USE_EXT_PUBLISHER"]:
@@ -250,10 +227,8 @@ def publish_sensor_data_ext(s: Sensor, connection, cursor):
     insert_list = []
 
     for msg in s.data_buffer:
-
         n_data = len(msg.data)
         r = range(0, n_data)
-
         for index in r:
             try:
                 data_val = int(msg.data[index])
